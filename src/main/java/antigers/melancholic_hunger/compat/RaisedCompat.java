4047685c0aa7cb7c03a33bae726ecc70af52145d
antigers <kaskadiggers@gmail.com>
2025-12-27 14:49:36 +0600

    Updated to 26.1-snapshot-1

--- src/main/java/antigers/melancholic_hunger/compat/RaisedCompat.java
+++ src/main/java/antigers/melancholic_hunger/compat/RaisedCompat.java
@@ -1,15 +1,15 @@
 package antigers.melancholic_hunger.compat;
 
-import dev.yurisuika.raised.registry.LayerRegistry;
-import dev.yurisuika.raised.util.Translate;
+//import dev.yurisuika.raised.registry.LayerRegistry;
+//import dev.yurisuika.raised.util.Translate;
 import net.minecraft.client.gui.GuiGraphics;
 
 public class RaisedCompat {
     public static void startHotbarTranslate(GuiGraphics guiGraphics) {
-        Translate.start(guiGraphics.pose(), LayerRegistry.HOTBAR);
+//        Translate.start(guiGraphics.pose(), LayerRegistry.HOTBAR);
     }
 
     public static void endTranslate(GuiGraphics guiGraphics) {
-        Translate.end(guiGraphics.pose(), LayerRegistry.HOTBAR);
+//        Translate.end(guiGraphics.pose(), LayerRegistry.HOTBAR);
     }
 }

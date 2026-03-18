4f4491c09c7710011993174dc612b049fd88e008
antigers <kaskadiggers@gmail.com>
2026-03-18 23:29:36 +0600

    Removed unnecessary Player.eat method injection

--- src/main/java/antigers/melancholic_hunger/mixin/PlayerMixin.java
+++ src/main/java/antigers/melancholic_hunger/mixin/PlayerMixin.java
@@ -1,23 +1,16 @@
 package antigers.melancholic_hunger.mixin;
 
-import antigers.melancholic_hunger.components.HealthRegenerationComponent;
 import antigers.melancholic_hunger.components.PlayerComponents;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Abilities;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.food.FoodData;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(Player.class)
 public abstract class PlayerMixin extends LivingEntity {
@@ -26,25 +19,6 @@ public abstract class PlayerMixin extends LivingEntity {
     private PlayerMixin(EntityType<? extends LivingEntity> entityType, Level world)
     {
         super(entityType, world);
-    }
-
-    /**
-     * Restores player's health after eating food
-     */
-    @WrapOperation(
-            method = "eat",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/food/FoodData;eat(Lnet/minecraft/world/food/FoodProperties;)V"
-            )
-    )
-    private void melancholic_hunger$playerEatFood(
-            FoodData foodData, FoodProperties foodProperties, Operation<Void> original, @Local(argsOnly = true) ItemStack itemStack
-    ) {
-        boolean didConsume = HealthRegenerationComponent.get((Player)(Object) this).eat(itemStack, foodProperties);
-        if (!didConsume) {
-            original.call(foodData, foodProperties);
-        }
     }
 
     /**

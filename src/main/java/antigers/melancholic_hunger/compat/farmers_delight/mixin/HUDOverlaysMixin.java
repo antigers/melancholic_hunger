c7bb182f08bdafdb45c7af5171cf17c4479e1530
antigers <kaskadiggers@gmail.com>
2026-03-21 22:55:15 +0600

    Improved compatibility with mods that use Fabric HUD rendering API

--- src/main/java/antigers/melancholic_hunger/compat/farmers_delight/mixin/HUDOverlaysMixin.java
+++ src/main/java/antigers/melancholic_hunger/compat/farmers_delight/mixin/HUDOverlaysMixin.java
@@ -39,6 +39,6 @@ public class HUDOverlaysMixin {
 			return;
 		}
 		DrawHudContext drawHudContext = (DrawHudContext) graphics;
-		original.call(foodData, minecraft, graphics, right, top + 7 - drawHudContext.getHudExperienceOffset(), naturalHealing);
+		original.call(foodData, minecraft, graphics, right, top - drawHudContext.getHudExperienceOffset(), naturalHealing);
 	}
 }

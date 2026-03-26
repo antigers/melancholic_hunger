59e78ae62d21e70390ccf013eb612a8268be7423
antigers <kaskadiggers@gmail.com>
2026-03-27 00:53:49 +0600

    Fixed custom item stack sizes not working

--- src/main/java/antigers/melancholic_hunger/food/mixin/ItemStackMixin.java
+++ src/main/java/antigers/melancholic_hunger/food/mixin/ItemStackMixin.java
@@ -3,12 +3,11 @@ package antigers.melancholic_hunger.food.mixin;
 import antigers.melancholic_hunger.config.MelancholicConfig;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import net.minecraft.world.item.ItemInstance;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin(ItemInstance.class)
-public interface ItemInstanceMixin {
+@Mixin(ItemStack.class)
+public class ItemStackMixin {
     @WrapMethod(method = "getMaxStackSize")
     private int melancholic_hunger$customStackSize(Operation<Integer> original) {
         if ((Object) this instanceof ItemStack itemStack) {

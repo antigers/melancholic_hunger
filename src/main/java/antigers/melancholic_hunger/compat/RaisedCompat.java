e749f5789d81d3a0568323582c185afa37f077a8
antigers <kaskadiggers@gmail.com>
2026-09-23 00:04:31 +0600

    Fixed crashing with Raised 6.0.0

--- src/main/java/antigers/melancholic_hunger/compat/RaisedCompat.java
+++ src/main/java/antigers/melancholic_hunger/compat/RaisedCompat.java
@@ -1,15 +1,14 @@
 package antigers.melancholic_hunger.compat;
 
-import dev.yurisuika.raised.registry.LayerRegistry;
 import dev.yurisuika.raised.util.Translate;
 import net.minecraft.client.gui.DrawContext;
 
 public class RaisedCompat {
     public static void startHotbarTranslate(DrawContext drawContext) {
-        Translate.start(drawContext.getMatrices(), LayerRegistry.HOTBAR);
+        Translate.start(drawContext.getMatrices(), "minecraft:hotbar");
     }
 
     public static void endTranslate(DrawContext drawContext) {
-        Translate.end(drawContext.getMatrices(), LayerRegistry.HOTBAR);
+        Translate.end(drawContext.getMatrices(), "minecraft:hotbar");
     }
 }
